6856da26a0748155a4b08bedb1f9d5ffacfac372
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-12-15 09:43:24 -0600

    added gsap library for animations

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,61 +1,50 @@
 <template>
   <div class="about">
   <transition 
-    appear 
-    name="fade"
-    @before-enter="beforeEnter"
-    @enter="enter"
-    @after-enter="afterEnter"
-    @before-leave="beforeLeave"
-    @leave="leave"
-    @after-leave="afterLeave"
-    >
-      <h1 v-if="showTitle">About</h1>
+		appear
+		@before-enter="beforeEnter"
+		@enter="enter"
+		@after-enter="afterEnter"
+		>
+      	<h1>About</h1>
   </transition>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import gsap from 'gsap'
  
 export default {
+
   setup() {
 
-    const showTitle = ref(true)
-
     const beforeEnter = (el) => {
-      console.log('before enter', el);
-    }
-    const enter = (el) => {
-      console.log('enter', el);
-    }
-    const afterEnter = (el) => {
-      el.style.color = 'green'
-      console.log('after enter', el);
-
-      setTimeout(() => {
-        showTitle.value = false
-      }, 2000);
-    }
-    const beforeLeave = (el) => {
-      el.style.color = 'pink'
-      console.log('before leave', el);
-    }
-    const leave = (el) => {
-      console.log('leave', el);
-    }
-    const afterLeave = (el) => {
-      console.log('after leave', el);
-
-      setTimeout(() => {
-        showTitle.value = false
-      }, 2000);
+    	console.log('before enter - set initial state');
+    	el.style.transform = 'translateY(-60px)'
+    	el.style.opacity = 0
     }
 
-    return { showTitle, beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave }
+    const enter = (el, done) => {
+		console.log('starting to enter - make transition');
+		// el.style.opcacity = 1
+		gsap.to(el, {
+			duration: 3,
+			y: 0,
+			opacity: 1,
+			ease: 'bounce.out',
+			onComplete: done
+		})
+    }
+
+	const afterEnter = () => {
+		console.log('after enter');
+	}
+  
+
+    return { beforeEnter, enter, afterEnter  }
   }
 
 }
